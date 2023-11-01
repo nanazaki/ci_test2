c1ce93c2a701c185c52f3cd53f8d907640cba7f9
nanazaki <120670073+nanazaki@users.noreply.github.com>
2023-11-01 13:24:38 +0900

    Update test_demo.py

--- test_demo.py
+++ test_demo.py
@@ -1,6 +1,7 @@
 import demo
 
 def test_demo_01():
-  assert demo.demofunc(1)==1
+  assert demo.demofunc(1) == 1
 def test_demo_02():
-  assert demo.demofunc(0)==0
+  assert demo.demofunc(0) == 0
+  
